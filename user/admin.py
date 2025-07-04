cc52748faa80efcdfa9cce64fa43fe3707dd409c
Igor Chapy <misha@mate.acedemy>
2025-07-04 19:36:08 +0300

    solution

--- user/admin.py
+++ user/admin.py
@@ -7,4 +7,5 @@ class CustomUserAdmin(BaseUserAdmin):
     ordering = ["email"]  # or any valid field from your User model
     list_display = ["email", "is_staff"]  # customize as needed
 
+
 admin.site.register(User, CustomUserAdmin)
