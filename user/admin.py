7507e60995be101a8bad6c8b91edbecf4e271f82
Igor Chapy <misha@mate.acedemy>
2025-07-04 19:35:30 +0300

    solution

--- user/admin.py
+++ user/admin.py
@@ -2,10 +2,9 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from .models import User
 
-class CustomUserAdmin(BaseUserAdmin):
-    ordering = ['email']  # or any valid field from your User model
-    list_display = ['email', 'is_staff']  # customize as needed
 
-    # If you use AbstractBaseUser, define form and add_fieldsets
+class CustomUserAdmin(BaseUserAdmin):
+    ordering = ["email"]  # or any valid field from your User model
+    list_display = ["email", "is_staff"]  # customize as needed
 
 admin.site.register(User, CustomUserAdmin)
