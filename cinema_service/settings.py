6edbbe4e0fd76ef6c99776045b9129ffde7c5f87
Igor Chapy <misha@mate.acedemy>
2025-07-04 20:34:20 +0300

    olution

--- cinema_service/settings.py
+++ cinema_service/settings.py
@@ -137,3 +137,9 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+STATIC_URL = "static/"
+
+MEDIA_ROOT = BASE_DIR / "media"
+
+MEDIA_URL = "/media/"
