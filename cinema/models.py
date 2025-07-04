1eb27136c5db2b6f58da21322761a8c886dea6d3
Igor Chapy <misha@mate.acedemy>
2025-07-04 19:32:25 +0300

    solution

--- cinema/models.py
+++ cinema/models.py
@@ -1,55 +1,20 @@
-from django.contrib.auth.models import AbstractUser, BaseUserManager
+from django.core.exceptions import ValidationError
 from django.db import models
-from django.utils.translation import gettext as _
+from django.conf import settings
 
 
-class UserManager(BaseUserManager):
-    """Define a model manager for User model with no username field."""
-    use_in_migrations = True
+class CinemaHall(models.Model):
+    name = models.CharField(max_length=255)
+    rows = models.IntegerField()
+    seats_in_row = models.IntegerField()
 
-    def _create_user(self, email, password, **extra_fields):
-        """Create and save a User with the given email and password."""
+    @property
+    def capacity(self) -> int:
+        return self.rows * self.seats_in_row
 
-        if not email:
-            raise ValueError("The given email must be set")
+    def __str__(self):
+        return self.name
 
-        email = self.normalize_email(email)
-        user = self.model(email=email, **extra_fields)
-        user.set_password(password)
-        user.save(using=self._db)
-        return user
-
-    def create_user(self, email, password=None, **extra_fields):
-        """Create and save a regular User with the given email and password."""
-
-        extra_fields.setdefault("is_staff", False)
-        extra_fields.setdefault("is_superuser", False)
-        return self._create_user(email, password, **extra_fields)
-
-    def create_superuser(self, email, password, **extra_fields):
-        """Create and save a SuperUser with the given email and password."""
-
-        extra_fields.setdefault("is_staff", True)
-        extra_fields.setdefault("is_superuser", True)
-
-        if extra_fields.get("is_staff") is not True:
-            raise ValueError("Superuser must have is_staff=True.")
-
-        if extra_fields.get("is_superuser") is not True:
-            raise ValueError("Superuser must have is_superuser=True.")
-
-        return self._create_user(email, password, **extra_fields)
-
-
-class User(AbstractUser):
-    pass
-    username = None
-    email = models.EmailField(_("email address"), unique=True)
-
-    USERNAME_FIELD = "email"
-    REQUIRED_FIELDS = []
-
-    objects = UserManager()
 
 class Genre(models.Model):
     name = models.CharField(max_length=255, unique=True)
